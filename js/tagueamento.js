4839245890c8715748c00ba36af316e33f8f087f
sanchezmichel <michel.sanchez@live.com>
2021-12-17 13:02:27 -0300

    Add files via upload

---
+++ js/tagueamento.js
@@ -0,0 +1,45 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+/*
+
+
+const contato = document.getElementById('menu_entrar_em_contato');
+contato.addEventListener('click',() => {
+  ga('send', 'event', {
+    'eventCategory': 'menu',
+    'eventAction': 'entre_em_contato',
+    'eventLabel': 'link_externo'
+  });  
+})
+
+
+
+const downloadpdf = document.getElementById('menu_download_pdf');
+downloadpdf.addEventListener('click',() => {
+  ga('send', 'event', {
+    'eventCategory': 'menu',
+    'eventAction': 'download_pdf',
+    'eventLabel': 'download_pdf'
+  });
+})
+
+
+
+const contato_nome = document.getElementById('nome');
+contato_nome.addEventListener('blur',() => {
+  if(contato_nome.value !== ""){
+    ga('send', 'event', {
+      'eventCategory': 'contato',
+      'eventAction': 'nome',
+      'eventLabel': 'preencheu'
+    });
+  }
+})
+
+*/
